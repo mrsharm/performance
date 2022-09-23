f56dfe80044011afa187ae30b581218d57f775e2
mrsharm <musharm@microsoft.com>
2022-09-23 13:50:28 -0700

    Added the Perf String tests and removed the overhead runs

--- src/benchmarks/micro/libraries/System.Runtime/Perf.String.cs
+++ src/benchmarks/micro/libraries/System.Runtime/Perf.String.cs
@@ -158,6 +158,35 @@ namespace System.Tests
         public string Replace_Char(string text, char oldChar, char newChar)
             => text.Replace(oldChar, newChar);
 
+        const string Input = @"""
+        Whose woods these are I think I know.
+        His house is in the village though;
+        He will not see me stopping here
+        To watch his woods fill up with snow.
+        My little horse must think it queer
+        To stop without a farmhouse near
+        Between the woods and frozen lake
+        The darkest evening of the year.
+        He gives his harness bells a shake
+        To ask if there is some mistake.
+        The only other sound’s the sweep
+        Of easy wind and downy flake.
+        The woods are lovely, dark and deep,
+        But I have promises to keep,
+        And miles to go before I sleep,
+        And miles to go before I sleep.
+        """;
+        private char[] _chars = Input.ToCharArray();
+
+        [Benchmark]
+        public string WithContent1() => new string(_chars);
+
+        [Benchmark]
+        public string WithoutContent2() => new string('\0', Input.Length);
+
+        [Benchmark]
+        public string Replace_Char_Custom() => Input.Replace('I', 'U');
+
         public static IEnumerable<object[]> ReplaceArguments()
         {
             yield return new object[] { "Hello", 'l', '!' };    // Contains two 'l'
--- src/harness/BenchmarkDotNet.Extensions/RecommendedConfig.cs
+++ src/harness/BenchmarkDotNet.Extensions/RecommendedConfig.cs
@@ -39,7 +39,9 @@ namespace BenchmarkDotNet.Extensions
                     .WithIterationTime(TimeInterval.FromMilliseconds(250)) // the default is 0.5s per iteration, which is slighlty too much for us
                     .WithMinIterationCount(15)
                     .WithMaxIterationCount(20) // we don't want to run more that 20 iterations
-                    .DontEnforcePowerPlan(); // make sure BDN does not try to enforce High Performance power plan on Windows
+                    .DontEnforcePowerPlan() // make sure BDN does not try to enforce High Performance power plan on Windows
+                    .WithEvaluateOverhead(false);
+                    //.WithGcForce(false);
             }
 
             if (resumeRun)
